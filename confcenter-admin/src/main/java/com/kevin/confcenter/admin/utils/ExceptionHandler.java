0e52b4c33bd8d557dd57f98c6de915818cafd16e
jizhong.zhai <jizhong.zhai@luckincoffee.com>
2018-04-16 11:26:08 +0800

    异常统一处理

---
+++ confcenter-admin/src/main/java/com/kevin/confcenter/admin/utils/ExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.kevin.confcenter.admin.utils;
+
+import com.kevin.confcenter.common.bean.vo.ResultInfo;
+import com.kevin.confcenter.common.exception.ConfCenterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: kevin
+ * @Description: 异常统一处理
+ * @Date: Created In 2018/4/16 10:25
+ */
+@ControllerAdvice
+public class ExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResultInfo handler(Exception e) {
+        if (e instanceof ConfCenterException) {
+            return ResultInfo.errorMessage(e.getMessage());
+        } else {
+            LOGGER.error("exception:{}", e.getMessage(), e);
+            return ResultInfo.errorMessage("服务器内部错误");
+        }
+    }
+}
