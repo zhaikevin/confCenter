4b618aa3b38c219c4f8534bcce574941ba6cb4e1
jizhong.zhai <jizhong.zhai@luckincoffee.com>
2018-06-12 10:19:56 +0800

    log

--- confcenter-admin/src/main/java/com/kevin/confcenter/admin/log/handler/user/UserModifyHandler.java
+++ confcenter-admin/src/main/java/com/kevin/confcenter/admin/log/handler/user/UserModifyHandler.java
@@ -6,8 +6,10 @@ import com.kevin.confcenter.admin.log.handler.HandlerName;
 import com.kevin.confcenter.admin.service.operation.UserService;
 import com.kevin.confcenter.common.bean.po.operation.OperationLog;
 import com.kevin.confcenter.common.bean.po.operation.User;
+import com.kevin.confcenter.common.consts.web.CommonStatusEnum;
 import com.kevin.confcenter.common.consts.web.operation.OperationTypeEnum;
 import com.kevin.confcenter.common.consts.web.operation.TargetTypeEnum;
+import com.kevin.confcenter.common.consts.web.operation.UserTypeEnum;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -67,19 +69,41 @@ public class UserModifyHandler extends AbstracterLogServiceHandler {
         StringBuilder change = new StringBuilder();
         if (StringUtils.isNotEmpty(newUser.getUserName()) &&
                 !newUser.getUserName().equals(oldUser.getUserName())) {
+            change.setLength(0);
             change.append("会员姓名从").append(oldUser.getUserName())
                     .append("修改为").append(newUser.getUserName());
+            changes.add(change.toString());
+        }
+        if (newUser.getStatus() != null &&
+                !newUser.getStatus().equals(oldUser.getStatus())) {
+            change.setLength(0);
+            change.append("会员状态从").append(CommonStatusEnum.getLabel(oldUser.getStatus()))
+                    .append("修改为").append(CommonStatusEnum.getLabel(newUser.getStatus()));
+            changes.add(change.toString());
+        }
+        if(newUser.getType() != null &&
+                !newUser.getType().equals(oldUser.getType())) {
+            change.setLength(0);
+            change.append("账户类型从").append(UserTypeEnum.getLabel(oldUser.getType()))
+                    .append("修改为").append(UserTypeEnum.getLabel(newUser.getType()));
+            changes.add(change.toString());
+        }
+        if (StringUtils.isNotEmpty(newUser.getMail()) &&
+                !newUser.getMail().equals(oldUser.getMail())) {
+            change.setLength(0);
+            change.append("邮箱从").append(oldUser.getMail())
+                    .append("修改为").append(newUser.getMail());
+            changes.add(change.toString());
         }
         if (changes.size() > 0) {
-            sb.append("修改用户信息，");
+            sb.append("修改用户信息：");
             for (int i = 0; i < changes.size(); i++) {
                 sb.append(changes.get(i));
                 if (i < changes.size() - 1) {
-                    sb.append("、");
+                    sb.append("，");
                 }
             }
         }
-
         return sb.toString();
     }
 }
