572b2301412dbeb37d561f285f29eda31f4c1661
jizhong.zhai <jizhong.zhai@luckincoffee.com>
2018-06-29 16:30:16 +0800

    删除无用的代码

--- view/src/views/common/table/multi-page-table.vue
+++ view/src/views/common/table/multi-page-table.vue
@@ -19,7 +19,7 @@
 const editButton = (vm, h, currentRow, index, item) => {
     return h('Button', {
         props: {
-            type: 'primary'
+            type: item.buttonType ? item.buttonType : 'primary'
         },
         style: {
             margin: '0 5px'
@@ -33,7 +33,7 @@ const editButton = (vm, h, currentRow, index, item) => {
                 }
             }
         }
-    }, '编辑');
+    }, item.name ? item.name : '编辑');
 };
 const deleteButton = (vm, h, currentRow, index, item) => {
     return h('Poptip', {
@@ -63,71 +63,6 @@ const deleteButton = (vm, h, currentRow, index, item) => {
             }, item.name ? item.name : '删除')
         ]);
 };
-const incellEditBtn = (vm, h, param) => {
-    if (vm.hoverShow) {
-        return h('div', {
-            'class': {
-                'show-edit-btn': vm.hoverShow
-            }
-        }, [
-                h('Button', {
-                    props: {
-                        type: 'text',
-                        icon: 'edit'
-                    },
-                    on: {
-                        click: (event) => {
-                            vm.edittingStore[param.index].edittingCell[param.column.key] = true;
-                            vm.thisTableData = JSON.parse(JSON.stringify(vm.edittingStore));
-                        }
-                    }
-                })
-            ]);
-    } else {
-        return h('Button', {
-            props: {
-                type: 'text',
-                icon: 'edit'
-            },
-            on: {
-                click: (event) => {
-                    vm.edittingStore[param.index].edittingCell[param.column.key] = true;
-                    vm.thisTableData = JSON.parse(JSON.stringify(vm.edittingStore));
-                }
-            }
-        });
-    }
-};
-const saveIncellEditBtn = (vm, h, param) => {
-    return h('Button', {
-        props: {
-            type: 'text',
-            icon: 'checkmark'
-        },
-        on: {
-            click: (event) => {
-                vm.edittingStore[param.index].edittingCell[param.column.key] = false;
-                vm.thisTableData = JSON.parse(JSON.stringify(vm.edittingStore));
-                vm.$emit('input', vm.handleBackdata(vm.thisTableData));
-                vm.$emit('on-cell-change', vm.handleBackdata(vm.thisTableData), param.index, param.column.key);
-            }
-        }
-    });
-};
-const cellInput = (vm, h, param, item) => {
-    return h('Input', {
-        props: {
-            type: 'text',
-            value: vm.edittingStore[param.index][item.key]
-        },
-        on: {
-            'on-change'(event) {
-                let key = item.key;
-                vm.edittingStore[param.index][key] = event.target.value;
-            }
-        }
-    });
-};
 export default {
     name: 'multiPageTable',
     props: {
@@ -144,7 +79,6 @@ export default {
             default:true
         },
         value: Array,
-        url: String,
         editIncell: {
             type: Boolean,
             default: false
@@ -158,7 +92,6 @@ export default {
         return {
             columns: [],
             thisTableData: [],
-            edittingStore: [],
             current: 1
         };
     },
@@ -168,89 +101,8 @@ export default {
     methods: {
         init() {
             let vm = this;
-            let editableCell = this.columnsList.filter(item => {
-                if (item.editable) {
-                    if (item.editable === true) {
-                        return item;
-                    }
-                }
-            });
-            let cloneData = JSON.parse(JSON.stringify(this.value));
-            let res = [];
-            res = cloneData.map((item, index) => {
-                let isEditting = false;
-                if (this.thisTableData[index]) {
-                    if (this.thisTableData[index].editting) {
-                        isEditting = true;
-                    } else {
-                        for (const cell in this.thisTableData[index].edittingCell) {
-                            if (this.thisTableData[index].edittingCell[cell] === true) {
-                                isEditting = true;
-                            }
-                        }
-                    }
-                }
-                if (isEditting) {
-                    return this.thisTableData[index];
-                } else {
-                    this.$set(item, 'editting', false);
-                    let edittingCell = {};
-                    editableCell.forEach(item => {
-                        edittingCell[item.key] = false;
-                    });
-                    this.$set(item, 'edittingCell', edittingCell);
-                    return item;
-                }
-            });
-            this.thisTableData = res;
-            this.edittingStore = JSON.parse(JSON.stringify(this.thisTableData));
+            this.thisTableData = JSON.parse(JSON.stringify(this.value));;
             this.columnsList.forEach(item => {
-                if (item.editable) {
-                    item.render = (h, param) => {
-                        let currentRow = this.thisTableData[param.index];
-                        if (!currentRow.editting) {
-                            if (this.editIncell) {
-                                return h('Row', {
-                                    props: {
-                                        type: 'flex',
-                                        align: 'middle',
-                                        justify: 'center'
-                                    }
-                                }, [
-                                        h('Col', {
-                                            props: {
-                                                span: '22'
-                                            }
-                                        }, [
-                                                !currentRow.edittingCell[param.column.key] ? h('span', currentRow[item.key]) : cellInput(this, h, param, item)
-                                            ]),
-                                        h('Col', {
-                                            props: {
-                                                span: '2'
-                                            }
-                                        }, [
-                                                currentRow.edittingCell[param.column.key] ? saveIncellEditBtn(this, h, param) : incellEditBtn(this, h, param)
-                                            ])
-                                    ]);
-                            } else {
-                                return h('span', currentRow[item.key]);
-                            }
-                        } else {
-                            return h('Input', {
-                                props: {
-                                    type: 'text',
-                                    value: currentRow[item.key]
-                                },
-                                on: {
-                                    'on-change'(event) {
-                                        let key = param.column.key;
-                                        vm.edittingStore[param.index][key] = event.target.value;
-                                    }
-                                }
-                            });
-                        }
-                    };
-                }
                 if (item.handle) {
                     item.render = (h, param) => {
                         let currentRowData = this.thisTableData[param.index];
